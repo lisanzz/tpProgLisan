b47aca33fbfb4ad6f625096be2ce0ddd8aac28fc
lisanzz <90874382+lisanzz@users.noreply.github.com>
2023-11-19 21:12:10 -0300

    Add files via upload

---
+++ CineCordobaBack/Entidades/Peliculas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineCordobaBack.Entidades
+{
+    public class Peliculas
+    {
+        public int PeliculaId { get; set; }
+        public string NombrePelicula { get; set; }
+
+        public Peliculas()
+        {
+        }
+    }
+}
---
+++ CineCordobaBack/Entidades/TipoDoc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineCordobaBack.Entidades
+{
+    public class TipoDoc
+    {
+        public int TipoDocId { get; set; }
+        public string TipoDocumento { get; set; }
+
+        public TipoDoc(int tipodocid,string tipoDocumento)
+        {
+            TipoDocId = tipodocid;
+            TipoDocumento = tipoDocumento;
+
+
+        }
+        public TipoDoc() 
+        { }
+
+    }
+}
